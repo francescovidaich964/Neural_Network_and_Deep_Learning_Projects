829a479c2a33d2de2c225e26f72ef270933c52b2
Francesco Vidaich <francesco.vidaich@gmail.com>
2020-01-12 18:18:21 +0100

    First day on Lab_06

--- Lab_06/agent.py
+++ Lab_06/agent.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.special as sp
+import matplotlib.pyplot as plt
 
 class Agent:
 
@@ -11,6 +12,7 @@ class Agent:
     softmax = False
     sarsa = False
     
+
     # initialize
     def __init__(self, states, actions, discount, max_reward, softmax, sarsa):
         self.states = states
@@ -22,22 +24,28 @@ class Agent:
         # initialize Q table
         self.qtable = np.ones([states, actions], dtype = float) * max_reward / (1 - discount)
         
+
     # update function (Sarsa and Q-learning)
     def update(self, state, action, reward, next_state, alpha, epsilon):
+
         # find the next action (greedy for Q-learning, using the decision policy for Sarsa)
-        next_action = self.select_action(next_state, 0)
         if (self.sarsa):
-            next_action = self.select_action(next_state, epsilon)
+            next_action = self.select_action(next_state, epsilon)  # eps-greedy or softmax
+        else:
+            next_action = self.select_action(next_state, 0)  # greedy
+
         # calculate long-term reward with bootstrap method
         observed = reward + self.discount * self.qtable[next_state, next_action]
         # bootstrap update
         self.qtable[state, action] = self.qtable[state, action] * (1 - alpha) + observed * alpha
         
+
     # action policy: implements epsilon greedy and softmax
     def select_action(self, state, epsilon):
         qval = self.qtable[state]
         prob = []
-        if (self.softmax):
+
+        if (self.softmax) and (epsilon!=0):
             # use Softmax distribution
             prob = sp.softmax(qval / epsilon)
         else:
@@ -45,4 +53,18 @@ class Agent:
             prob = np.ones(self.actions) * epsilon / (self.actions - 1)
             # the best action is taken with probability 1 - epsilon
             prob[np.argmax(qval)] = 1 - epsilon
+
         return np.random.choice(range(0, self.actions), p = prob)
+
+
+    # Function that plots a visualization of the estimated values of each state
+    def visualize_values(self, x, y, filename=None):
+
+        #Plot values, computed as the max Q-value of every state
+        plt.imshow(self.qtable.max(axis=1).reshape(x,y))
+        plt.colorbar()
+        plt.title("Value map")
+        if filename==None:
+            plt.show()
+        else:
+            plt.savefig(filename)
--- Lab_06/training.py
+++ Lab_06/training.py
@@ -1,50 +1,100 @@
+
 import dill
 import numpy as np
+import matplotlib.pyplot as plt
 import agent
 import environment
 
-episodes = 2000         # number of training episodes
-episode_length = 50     # maximum episode length
-x = 10                  # horizontal size of the box
-y = 10                  # vertical size of the box
-goal = [0, 3]           # objective point
-discount = 0.9          # exponential discount factor
-softmax = False         # set to true to use Softmax policy
-sarsa = False           # set to true to use the Sarsa algorithm
 
-# TODO alpha and epsilon profile
-alpha = np.ones(episodes) * 0.25
-epsilon = np.linspace(0.8, 0.001,episodes)
-
-# initialize the agent
-learner = agent.Agent((x * y), 5, discount, max_reward=1, softmax=softmax, sarsa=sarsa)
-
-# perform the training
-for index in range(0, episodes):
-    # start from a random state
-    initial = [np.random.randint(0, x), np.random.randint(0, y)]
-    # initialize environment
-    state = initial
-    env = environment.Environment(x, y, state, goal)
-    reward = 0
-    # run episode
-    for step in range(0, episode_length):
-        # find state index
-        state_index = state[0] * y + state[1]
-        # choose an action
-        action = learner.select_action(state_index, epsilon[index])
-        # the agent moves in the environment
-        result = env.move(action)
-        # Q-learning update
-        next_index = result[0][0] * y + result[0][1]
-        learner.update(state_index, action, result[1], next_index, alpha[index], epsilon[index])
-        # update state and reward
-        reward += result[1]
-        state = result[0]
-    reward /= episode_length
-    print('Episode ', index + 1, ': the agent has obtained an average reward of ', reward, ' starting from position ', initial) 
+# Function that trains the agent in input
+def train_agent(learner, x, y, initial, goal, episodes, eps_decay_exp, log=True,
+                discount=0.9, ep_per_epoch=100, episode_length=50):
     
-    # periodically save the agent
-    if ((index + 1) % 10 == 0):
-        with open('agent.obj', 'wb') as agent_file:
-            dill.dump(agent, agent_file)
+    # TODO alpha and epsilon profile
+    alpha = np.ones(episodes) * 0.25
+    #epsilon = np.linspace(0.8, 0.001,episodes)
+    epsilon = np.linspace(0.3, 0.001,episodes)
+
+
+    # perform the training
+    mean_rewards = np.zeros(episodes//ep_per_epoch)
+    for index in range(0, episodes):
+
+        # start from a random state if it was set to None
+        if initial == None:
+            initial = [np.random.randint(0, x), np.random.randint(0, y)]
+        # initialize environment
+        state = initial
+        env = environment.Environment(x, y, state, goal)
+        reward = 0
+
+        # run episode
+        for step in range(0, episode_length):
+
+            # find state index
+            state_index = state[0] * y + state[1]
+            # choose an action
+            action = learner.select_action(state_index, epsilon[index])
+            # the agent moves in the environment
+            result = env.move(action)
+            # Q-learning update
+            next_index = result[0][0] * y + result[0][1]
+            learner.update(state_index, action, result[1], next_index, alpha[index], epsilon[index])
+            # update state and reward
+            reward += result[1]
+            state = result[0]
+
+        reward /= episode_length
+        mean_rewards[index//ep_per_epoch] += reward/ep_per_epoch
+
+        # periodically save the agent and print results
+        if ((index + 1) % ep_per_epoch == 0):
+            with open('agent.obj', 'wb') as agent_file:
+                dill.dump(agent, agent_file)
+            
+            if log:
+                print('Episodes '+str(index-99)+'-'+str(index+1)+': Mean reward per step is', np.round(mean_rewards[index//ep_per_epoch],4))
+            
+    return mean_rewards
+
+
+
+
+# Function that train an agent using all the possible combinations
+# of algorithm (Q-learning/SARSA) and policy (eps-greedy/softmax)
+def compare_algorithms_and_policies(x, y, initial, goal, episodes, discount=0.9, ep_per_epoch=100):
+
+    # Test the 4 possible combinations of algorithm and policy
+    for case in range(4):
+        
+        # Set algorithm
+        sarsa = case//2 
+        if sarsa:
+            algorithm = 'SARSA'
+        else:
+            algorithm = 'Q-learning'
+            
+        # Set policy
+        softmax = case%2
+        if softmax:
+            policy = 'Softmax'
+        else:
+            policy = 'eps-greedy'
+        
+
+        # Initialize the agent (grid with 'x*y' states and 5 possible actions)
+        combination = algorithm +' + '+ policy
+        print('\nTraining agent using ', algorithm+' + '+policy)
+        learner = agent.Agent((x * y), 5, discount, max_reward=1, softmax=softmax, sarsa=sarsa)
+        
+        # Train model and store the Mean reward per step
+        mean_rewards_per_step = train_agent(learner, x, y, initial, goal, episodes, log=False, \
+                                            discount=discount, ep_per_epoch=ep_per_epoch)
+        plt.plot( np.arange(episodes//ep_per_epoch)*ep_per_epoch, mean_rewards_per_step, label=combination)
+        print('Final value of mean reward per step is', np.round(mean_rewards_per_step[-1],4))
+        
+    # Plot evolution of mean reward per step during training of each combination
+    plt.xlabel('Episodes')
+    plt.ylabel('Mean Reward per step')
+    plt.legend()
+    plt.show()
